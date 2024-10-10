fb1171eb8e7c0a1055eee29aaf8340a3ed4bc1e0
Sunny <136143741+sunnyajit@users.noreply.github.com>
2024-10-10 20:31:40 +0530

    Time: 2 ms (43.71%), Space: 8.5 MB (75.47%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.cpp
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -1,23 +1,15 @@
- class Solution {
-public:
-    int scoreOfString(string s) {
-        int score=0;
-        for(size_t i=1; i<s.length(); ++i){
-            score+=abs(s[i]-s[i-1]);
-        }
-        return score;
-    }
+ #include<string>
+#include<cmath>
 
-    int main(){
-        int t;
-        cin >> t;
-        while(t--){
-            string s;
-            cin >> s;
-            cout << scoreOfString(s) <<endl;
-        }
-         
-        return 0;
-    }
-
+class Solution{
+    public: 
+      int scoreOfString(std::string str){
+          int score = 0;
+          for(int i=1; i<str.size(); i++){
+              score+=abs(str[i]-str[i-1]);
+              
+          }
+          
+          return score;
+      }
 };
