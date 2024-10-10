99538a02fb57eb76a67858780f2b31166ebd9ea8
Sunny <136143741+sunnyajit@users.noreply.github.com>
2024-10-10 18:53:05 +0530

    Time: 24 ms (96.28%), Space: 36.1 MB (86.21%) - LeetHub

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
@@ -0,0 +1,18 @@
+ class Solution {
+public: // code written by sunny..
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int m = rowSum.size();
+        int n = colSum.size();
+        vector<vector<int>> mat(m, vector<int>(n, 0));
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                mat[i][j] = std::min(rowSum[i], colSum[j]);
+                rowSum[i] -= mat[i][j];
+                colSum[j] -= mat[i][j];
+            }
+        }
+        
+        return mat;
+    }
+};
