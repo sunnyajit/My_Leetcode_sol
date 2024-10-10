881b69fe5b29cda712e2f7a1e1efa0b44acec070
Sunny <136143741+sunnyajit@users.noreply.github.com>
2024-10-10 20:15:19 +0530

    Time: 4 ms (59.42%), Space: 15.9 MB (48.73%) - LeetHub

---
+++ 1021-distribute-coins-in-binary-tree/1021-distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,19 @@
+ class Solution {
+ public: // code written by sunny
+  int distributeCoins(TreeNode* root) {
+    int ans = 0;
+    dfs(root, ans);
+    return ans;
+  }
+
+ private:
+  int dfs(TreeNode* root, int& ans) {
+    if (root == nullptr)
+      return 0;
+
+    const int l = dfs(root->left, ans);
+    const int r = dfs(root->right, ans);
+    ans += abs(l) + abs(r);
+    return (root->val - 1) + l + r;
+  }
+};
