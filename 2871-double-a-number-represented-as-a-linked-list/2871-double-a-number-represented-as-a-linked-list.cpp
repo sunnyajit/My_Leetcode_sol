22138c639d8112ac4e8b43a74e651ea100af6cf2
Sunny <136143741+sunnyajit@users.noreply.github.com>
2024-10-11 14:11:20 +0530

    Time: 183 ms (84.29%), Space: 122.6 MB (39.32%) - LeetHub

---
+++ 2871-double-a-number-represented-as-a-linked-list/2871-double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ class Solution {  // written by sunny
+ public:
+  ListNode* doubleIt(ListNode* head) {
+    if (getCarry(head) == 1)
+      return new ListNode(1, head);
+    return head;
+  }
+
+ private: // code written by sunny
+  int getCarry(ListNode* node) {
+    int val = node->val * 2;
+    if (node->next != nullptr)
+      val += getCarry(node->next);
+
+    node->val = val % 10;
+    return val / 10;
+  }
+};
