3cb627b756992b227de14ff037f5115a183b4a1f
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-04 21:33:41 -0800

    Time: 0 ms (100.00%), Space: 12.2 MB (16.88%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,13 +1,16 @@
 class Solution {
-public: // code written by sunny
+public:
     int minChanges(string s) {
-         int n = s.length();
-        int changesToMakeUniformBlocks = 0;
-        for (int i = 0; i < n; i += 2) {
-            if (s[i] != s[i + 1]) {
-                changesToMakeUniformBlocks++;
-            }
-        }
-        return changesToMakeUniformBlocks;
+        int n = s.length();
+        int countblockmakestring = 0;
+          for(int i=0; i<n; i+=2){ // n tk maximum ja sksyyan
+              if(s[i]!=s[i+1]){
+                  countblockmakestring++; // x  = x+0=x=
+              }
+          }
+        // usko return kr denge jo change hua hai...
+        // O(n);
+        // O(1); no extra space ko count kiya gya hai...
+          return countblockmakestring;
     }
 };
