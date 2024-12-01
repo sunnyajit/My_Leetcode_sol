f0f1deb46d3617d49ffd71f4ca78907ea52cc437
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-12-01 09:07:47 +0530

    Time: 3 ms (24.95%), Space: 13.6 MB (16.68%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,16 @@
+ 
+
+class Solution {
+public: // code written by sunny
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> seen;
+        for (int num : arr) {
+            if (seen.count(2 * num) || (num % 2 == 0 && seen.count(num / 2))) {
+                return true;
+            } 
+            seen.insert(num);
+        }
+        return false;  
+    }
+};
+ 
