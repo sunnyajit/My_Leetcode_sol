969af4a98e1397b2d517a5787c463cde68da1b7f
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-12-18 17:31:21 +0530

    Time: 0 ms (100.00%), Space: 14.2 MB (17.75%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public: // code written by sunny
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> result(prices); 
+        stack<int> st;  
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && prices[st.top()] >= prices[i]) {
+                result[st.top()] -= prices[i];  
+                st.pop();  
+            }
+            st.push(i);  
+        }
+        
+        return result;
+    }
+};
