92d7ab74eee53ceeb390f4120feacc75bfa3a4a5
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-12-30 18:42:07 +0530

    Time: 3 ms, Memory: 11.6 MB - LeetHub

---
+++ 1975-180-2466-count-ways-to-build-good-strings/1975-180-2466-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public: // code written by sunny
+    int countGoodStrings(int low, int high, int zero, int one) {
+        const int MOD = 1e9 + 7;
+        vector<int> dp(high + 1, 0);
+        dp[0] = 1;  
+        
+        for (int i = 1; i <= high; ++i) {
+            if (i >= zero) {
+                dp[i] = (dp[i] + dp[i - zero]) % MOD;
+            }
+            if (i >= one) {
+                dp[i] = (dp[i] + dp[i - one]) % MOD;
+            }
+        }
+        
+        int result = 0;
+        for (int i = low; i <= high; ++i) {
+            result = (result + dp[i]) % MOD;
+        }
+        
+        return result;
+    }
+};
