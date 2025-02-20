61eadbe54b16582ef0dfc8be983d66af002cca0c
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2025-02-20 16:35:40 +0530

    Time: 0 ms, Memory: 12.6 MB - LeetHub

---
+++ 2238-83-1980-find-unique-binary-string/2238-83-1980-find-unique-binary-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public: // code written by sunny
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        string result = ""; 
+        for (int i = 0; i < n; i++) {
+            result += (nums[i][i] == '0') ? '1' : '0';
+        }
+        
+        return result;
+    }
+};
